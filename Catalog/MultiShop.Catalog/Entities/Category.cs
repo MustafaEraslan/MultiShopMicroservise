876c63b9bd9fa6e990b2eb895d3e912147cc48c1
mustafaeraslan <eraslan.mustafa@outlook.com>
2024-11-07 19:43:02 +0300

    added was mulitshop catalog

--- Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategorName { get; set; }
     }
 }
