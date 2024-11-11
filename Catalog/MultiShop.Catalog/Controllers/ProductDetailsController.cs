8cdd6e59dda5d11974afc5048f4928f632047538
mustafaeraslan <eraslan.mustafa@outlook.com>
2024-11-11 16:25:17 +0300

    swagger control was passed

---
+++ Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductDetailDetailServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _productDetailservice;
+
+        public ProductDetailsController(IProductDetailService ProductDetailservice)
+        {
+            _productDetailservice = ProductDetailservice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailList()
+        {
+            var values = await _productDetailservice.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetailById(string id)
+        {
+            var values = await _productDetailservice.GetByIdProductDetailAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+        {
+            await _productDetailservice.CreateProductDetailAsync(createProductDetailDto);
+            return Ok("Ürün detayı Başarılı bir şekilde eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetail(string id)
+        {
+            await _productDetailservice.DeleteProductDetailAsync(id);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            await _productDetailservice.UpdateProductDetailAsync(updateProductDetailDto);
+            return Ok("Ürün detayı başarılı bir şekilde güncellendi.");
+        }
+    }
+}
---
+++ Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImagesDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _productImagesService;
+
+        public ProductImagesController(IProductImageService ProductImagesService)
+        {
+            _productImagesService = ProductImagesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImagesList()
+        {
+            var values = await _productImagesService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImagesById(string id)
+        {
+            var values = await _productImagesService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImages(CreateProductImageDto createProductImagesDto)
+        {
+            await _productImagesService.CreateProductImageAsync(createProductImagesDto);
+            return Ok("Ürün görselleri Başarılı bir şekilde eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImages(string id)
+        {
+            await _productImagesService.DeleteProductImageAsync(id);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImages(UpdateProductImageDto updateProductImagesDto)
+        {
+            await _productImagesService.UpdateProductImageAsync(updateProductImagesDto);
+            return Ok("Ürün görselleri başarılı bir şekilde güncellendi.");
+        }
+    }
+}
